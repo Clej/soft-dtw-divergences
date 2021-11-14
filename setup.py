dd6def063b3496bbe75a05dd5ecb6d4f3a94c14f
Clément Lejeune <54889281+Clej@users.noreply.github.com>
2021-11-14 17:26:54 +0100

    update setup.py for cython compilation

--- setup.py
+++ setup.py
@@ -12,19 +12,23 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# forket from google-research/soft-dtw-divergences
 """Install sdtw_div."""
 
 from setuptools import find_packages
 from setuptools import setup
+from setuptools import Extension
+import numpy as np
+
 
 setup(
     name='sdtw_div',
-    version='0.1',
+    version='0.1dev',
     description=(
         'Differentiable Divergences between Time Series.'),
     author='Google LLC',
     author_email='no-reply@google.com',
-    url='https://github.com/google-research/soft-dtw-divergences',
+    url='https://github.com/clej/soft-dtw-divergences',
     license='Apache2',
     packages=find_packages(),
     package_data={},
@@ -39,4 +43,11 @@ setup(
         'Topic :: Scientific/Engineering :: Artificial Intelligence',
     ],
     keywords='machine learning time series dtw',
+    ext_modules=[
+        Extension('sdtw_div.sdtw_fast',
+                sources=['sdtw_div/sdtw_fast.pyx'],
+                language='c',
+                include_dirs=[np.get_include()]
+                )],
+    zip_safe=False
 )
